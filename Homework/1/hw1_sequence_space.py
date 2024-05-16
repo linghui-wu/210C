d130b6a7da096c52f13a12aec1b9185d6e4d2aef
linghui-wu <liw036@ucsd.edu>
2024-05-15 21:06:57 -0700

    Update hw1

--- Homework/1/hw1_sequence_space.py
+++ Homework/1/hw1_sequence_space.py
@@ -1,102 +1,153 @@
-# This script attempts to solve an RBC model using the
-# Sequence-Space-Jacobian package 
-# (https://github.com/shade-econ/sequence-jacobian/tree/ac77f11b90fc70b94633de0526ffa40fec046966)
-# 
-
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
-from scipy.optimize import fsolve, root
-from sequence_jacobian.utilities.drawdag import drawdag
-from sequence_jacobian import simple, create_model
-
-
-# Firm block
-@simple 
-def firm(A, N, P):
-    Y = A * N
-    W = A * P
-    return Y, W
-
-# Household block
-@simple 
-def household(M, P, W, chi, N, phi, theta, nu, gamma):
-    
-    def labor_leisure(C, W, P, chi, N, phi, theta, nu, gamma):
-        X = ((1-theta) * C ** (1-nu) + theta * (M/P) ** (1-nu)) ** (1/(1-nu))
-        return (W / P) - (chi * N**phi) / ((1 - theta) * C**(-nu) * X**(-gamma + nu))
-
-    def solve_for_C(W, P, chi, N, phi, theta, nu, gamma, initial_guess=1.0):
-        # Use fsolve to solve for C_t
-        C_solution = fsolve(labor_leisure, initial_guess, args=(W, P, chi, N, phi, theta, nu, gamma))
-        return C_solution[0]
-    
-    C = solve_for_C(W, P, chi, N, phi, theta, nu, gamma)
-    Q = 1 / (1 - ((1-theta)/theta*M/P/C)**(-nu))
-    X = ((1-theta) * C ** (1-nu) + theta * (M/P) ** (1-nu)) ** (1/(1-nu))
-    
-    return C, Q, X
-
-# Market clearings
-@simple
-def mkt_clearing(Y, C, Q, P, X, beta):
-    goods_mkt = Y - C
-    euler = beta * Q * P / P(+1) * (X(+1) / X) ** (-gamma + nu) * (C(+1) / C) ** (-nu)
-    return goods_mkt, euler
-
-
-# Set up RBC model
-rbc = create_model([household, firm, mkt_clearing], name="RBC")
-print(rbc)
-print(f"Blocks: {rbc.blocks}")
-
-
-# Draw DAG 
-unknowns = ['N', 'P']
-targets = ['euler', 'goods_mkt']
-inputs = ['M']
-
-drawdag(rbc, inputs, unknowns, targets)
-
 
 # Parameters
-gamma = 1
+gam = 1
 phi = 1
 chi = 1
 beta = 0.99
 rho = 0.99
-sigma_m = 0.01
-nu_list = [0.25, 0.5, 0.999, 2, 4]
+
+T = 500
+
+# Calibrating theta
+nu_arr = [0.25, 0.5, 0.999, 2, 4]
+theta_arr = []
+for nu in nu_arr:
+    theta_arr.append((1 - beta) / ( (1 - beta) + ( (-(1 - beta) + np.sqrt(beta**2 - 2*beta + 5))/2 )**nu))
+
+# Useful Matrices
+I = sp.sparse.eye(T)
+Ip1 = sp.sparse.diags([np.ones(T-1)], [1], (T, T))
+Im1 = sp.sparse.diags([np.ones(T-1)], [-1], (T, T))
+O = sp.sparse.csr_matrix((T, T))
+
+# m
+m = np.zeros((T, 1))
+m[0] = 1
+for t in range(1, T):
+    m[t] = rho * m[t-1]
+
+# Iteration
+## Record c, p, q IRFs
+C, P, Q = [], [], []
+for i in range(len(nu_arr)):
+    nu = nu_arr[i]
+    theta = theta_arr[i]
+
+    # Market Clearing Condition
+    Phigmc = I
+    Phigmy = -I
+    Phigmq = O
+    Phigmx = O
+    Phigmw = O
+
+    # Euler Equation
+    Phieulc = nu*I-nu*Ip1
+    Phieulx = (gam-nu)*I-(gam-nu)*Ip1
+    Phieulq = I
+    Phieuly = O
+    Phieulw = O
+
+    dHdY = sp.sparse.bmat([[Phigmc, Phigmq, Phigmx, Phigmy, Phigmw],
+                       [Phieulc, Phieulq, Phieulx, Phieuly, Phieulw]])
+
+    # dHdU
+    Phigmn = O
+    Phieuln = O
+    Phigmp = O
+    Phieulp = I-Ip1
+    dHdU = sp.sparse.bmat([[Phigmn, Phigmp],
+                       [Phieuln, Phieulp]])
+
+    # dYdU and dYdZ
+    Phiyn = I
+    Phiyp = O
+    Phiyep = O
+    Phiwp = I
+    Phiwn = O
+    Phiwep = O
+    dYFdU = sp.sparse.bmat([[Phiyn, Phiyp],
+                        [Phiwn, Phiwp]])
+    dYFdZ = sp.sparse.bmat([[Phiyep],
+                        [Phiwep]])
+    omega = (theta**(1/nu)*(1-beta)**(1-1/nu))/((1-theta)**(1/nu)+theta**(1/nu)*(1-beta)**(1-1/nu))
+    eta = beta/(nu*(1-beta))
+    Phicn = -eta*phi/(gam*eta-(gam-nu)*omega*eta)*I
+    Phicp = (gam-nu)*omega*eta/(gam*eta-(gam-nu)*omega*eta)*I
+    Phiqn = -phi/(gam*eta-(gam-nu)*omega*eta)*I
+    Phiqp = gam/(gam*eta-(gam-nu)*omega*eta)*I
+    Phicm = -(gam-nu)*omega*eta/(gam*eta-(gam-nu)*omega*eta)*I
+    Phiqm = -gam/(gam*eta-(gam-nu)*omega*eta)*I
+    Phixn = (1-omega)*Phicn
+    Phixp = (1-omega)*Phicp - omega*I
+    Phixm = (1-omega)*Phicm + omega*I
+    dYHdU = sp.sparse.bmat([[Phicn, Phicp],
+                        [Phiqn, Phiqp],
+                        [Phixn, Phixp]])
+    dYHdZ = sp.sparse.bmat([[Phicm],
+                        [Phiqm],
+                        [Phixm]])
+    dYdU = sp.sparse.bmat([[dYHdU],
+                           [dYFdU]])
+    dYdZ = sp.sparse.bmat([[dYHdZ],
+                       [dYFdZ]])
+
+    # dHdU and dHdZ
+    # Note that here dHdU has additional direct effect term
+    dHdU = dHdY @ dYdU + dHdU
+    dHdZ = dHdY @ dYdZ
+
+    # Jacobian
+    dUdZ = - sp.sparse.linalg.spsolve(dHdU, dHdZ)
+    dYdZ = dYdU @ dUdZ + dYdZ
+    dXdZ = sp.sparse.bmat([[dUdZ],
+                           [dYdZ]])
+    X = dXdZ @ m
+    # unpack X into its components n,q,c,q,x,y,w
+    n = X[0:T]
+    p = X[T:2*T]
+    c = X[2*T:3*T]
+    q = X[3*T:4*T]
+    x = X[4*T:5*T]
+    y = X[5*T:6*T]
+    w = X[6*T:7*T]
+
+    # Record
+    C.append(c)
+    P.append(p)
+    Q.append(q)
+
+plt.figure(figsize=(10,7))
+for i in range(len(C)):
+    plt.plot(C[i], label='nu='+str(nu_arr[i]))
+plt.xlabel('Time')
+plt.ylabel('Consumption')
+plt.title('Consumption Change')
+plt.legend()
+plt.show()
+plt.savefig('figs/irf_c.png')
 
 
-# Calibration
-nu_len = len(nu_list)
-theta_list = np.zeros(nu_len)
-
-def solve_for_theta(theta, nu):
-    return (1 - beta)**(-1 / nu) * (theta / (1 - theta))**(1 / nu) * ((1 - theta) / chi * ((1 - theta + theta * (1 - beta)**(-(1 - nu) / nu) * (theta / (1 - theta))**((1 - nu) / nu)))**((nu - gamma) / (1 - nu)))**(1 / (phi + gamma)) - 1
-
-for i in range(nu_len):
-    nu = nu_list[i]
-    sol = root(solve_for_theta, 0.01, args=(nu))
-    theta_list[i] = sol.x[0]
-
-calibration = {"N": 1, "A": 1, "M": 1, "P": 1, "r": 0.01, "gamma": 1, 
-               "chi": 1, "phi": 1, "nu": nu_list, 
-               "beta": 0.99, "theta": theta_list}
-unknowns_ss = {"C": 0, "Q": 0.5}
-targets_ss = {"goods_mkt": 0, "euler": 0}
+plt.figure(figsize=(10,7))
+for i in range(len(P)):
+    plt.plot(P[i], label='nu='+str(nu_arr[i]))
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.title('Price Change')
+plt.legend()
+plt.show()
+plt.savefig('figs/irf_p.png')
 
 
-# Solve for steady state
-ss = rbc.solve_steady_state(calibration, unknowns_ss, targets_ss, solver="hybr")
-
-
-# Solve for impulse responses
-G = rbc.solve_jacobian(ss, unknowns, targets, inputs, T=300)
-T, impact, rho = 300, 0.01, 0.8
-dM = np.empty((T, 1))
-dM[:, 0] = impact * ss['M'] * rho ** np.arange(T)
-dC = 100 * G['C']['M'] @ dM / ss['C']
-dP = 100 * G['P']['M'] @ dM / ss['C']
-dQ = 100 * G['Q']['M'] @ dM / ss['C']
+plt.figure(figsize=(10,7))
+for i in range(len(Q)):
+    plt.plot(Q[i], label='nu='+str(nu_arr[i]))
+plt.xlabel('Time')
+plt.ylabel('Nominal Interest Rate')
+plt.title('Nominal Interest Rate Change')
+plt.legend()
+plt.show()
+plt.savefig('figs/irf_q.png')
 
